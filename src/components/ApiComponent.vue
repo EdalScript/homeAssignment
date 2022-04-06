8d24ad9cf1282fad81642056a568a5f59154912b
Ed Torres <edalscript@gmail.com>
2022-04-06 21:57:06 +0300

    iteration over color and status_name in main component

--- src/components/ApiComponent.vue
+++ src/components/ApiComponent.vue
@@ -1,13 +1,23 @@
 <template>
   <h1>Data from API</h1>
-  <button class="btn">Waiting</button>
-  <div>
-     <ul v-if="statuses && statuses.length">
-      <li v-for="[status_id, status_name] in statuses" :key="status_id">
-        {{ status_id }} is {{ status_name}}
-      </li>
-    </ul>
-  </div>
+
+  <main>
+    <div class="list-container">
+      <button class="btn">Waiting</button>
+      <div class="dropdown">
+        <ul>
+          <li v-for="(status, index) in statuses.data" :key="index">
+            <span
+
+              class="status-color"
+              :style="{ 'background-color': status.color }"
+            ></span>
+            {{ status.status_name }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -17,7 +27,7 @@ const apiKey = "BAR";
 const config = {
   method: "POST",
   body: {
-    "lang": "eng",
+    lang: "eng",
   },
 };
 
@@ -25,14 +35,13 @@ export default {
   name: "api-component",
   data() {
     return {
-      data: null,
       statuses: [],
     };
   },
   async created() {
     try {
       const response = await axios
-        .post("/api/v2/statuses/list", {company_account_id, apiKey, config})
+        .post("/api/v2/statuses/list", { company_account_id, apiKey, config })
         .then((response) => (this.statuses = response.data));
       console.log(response.data);
     } catch (e) {
@@ -40,24 +49,45 @@ export default {
       console.log(e);
     }
   },
-  //Hi Joosep! Thanks for the tip, it did help a lot! 
-  //I'm having a lot of fun working in this homework and funnily enough it's just a homework-- yet quite challenging.
-  //I spent too much time understanding the API, how embarrassing! But I learned a lot thanks to this experience. 
-  //I'm quite stubborn myself to let this homework unfinished so I will continue working on this even if it's late
 };
 </script>
 
 <style scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 h3 {
   margin: 40px 0 0;
 }
 ul {
   list-style-type: none;
   padding: 0;
+  position: relative;
+  padding: 0;
+  box-shadow: 0px 0px 30px 15px rgba(97, 93, 93, 0.055);
+  width: 250px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  padding: 0.7em;
+  list-style: none;
+  vertical-align: middle;
+  width: 90%;
+}
+
+li:hover {
+  background-color: rgba(154, 194, 201, 0.055);
+}
+
+li:active {
+  background-color: rgba(96, 201, 219, 0.11);
+  border-left: 3px solid rgb(20, 147, 197);
 }
 a {
   color: #47c1bf;
@@ -72,5 +102,13 @@ a {
 .btn:hover {
   background: #47c1bf;
   color: #fff;
+}
+.status-color {
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  display: inline-block;
+  border: 4px solid white;
+  vertical-align: middle;
 }
 </style>
